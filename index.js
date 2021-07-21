277ae8da950f630aa8e8be9b91ab8fe13ca25b7d
Jody Lorist <jodylorist@hotmail.com>
2021-07-22 00:05:19 +0200

    server done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log("https://localhost:${PORT}");
+});
